76dfac4f04a560c866c74371f1b424b9a84b6887
Jose Andres <Gorusuke123@gmail.com>
2021-03-29 22:54:32 -0500

    Probando las variables de entorno con nextJs

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,15 +1,7 @@
 import firebase from "firebase"
 import "firebase/firestore"
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBKBZdYMIQLElDMTSpNjhnZnLAPJZHdaB4",
-  authDomain: "devtter-1be58.firebaseapp.com",
-  projectId: "devtter-1be58",
-  storageBucket: "devtter-1be58.appspot.com",
-  messagingSenderId: "205274350334",
-  appId: "1:205274350334:web:61d66eb3216e0fbf195baf",
-  measurementId: "G-NLR38DHS94",
-}
+const firebaseConfig = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG)
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
